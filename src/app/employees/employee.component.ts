6d53e98b0c4fa6146f6f835717eeb99f87713e67
Subin Sunu Jacob <subin03061998@gmail.com>
2020-05-20 10:29:43 +0530

    remaining comments in employee component

--- src/app/employees/employee.component.ts
+++ src/app/employees/employee.component.ts
@@ -1,16 +1,34 @@
+
+/*  
+  =======================================================================================================
+    Developer: Subin Sunu Jacob
+    Creation Date: 18th May,2020
+    Description: This is a base component s
+  ==========================================================================================================
+*/
+
+
+//import all the required entities from their respective packages
+
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+//decorator used for storing Component's metadata
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
   styleUrls: ['./employee.component.css']
 })
+
+//Component that handles all the tasks in correspondance to the html template
 export class EmployeeComponent implements OnInit {
+
+    //constructor used for injecting dependency
   constructor(private route: Router) { }
-  router: Router;
+
+  //ngOnInit used for initialising properties of the class
   ngOnInit(): void {
-    this.router = this.route;
+
   }
 
 }
--- src/app/employees/employee.model.ts
+++ src/app/employees/employee.model.ts
@@ -1,23 +1,36 @@
+
+/*  
+  =======================================================================================================
+    Developer: Subin Sunu Jacob
+    Creation Date: 16th May,2020
+    Description: This is a model (indicates a single employee) class of type Employee to be used as a type by the ouside classes/components
+ ==========================================================================================================
+*/
+
 export class Employee{
+
+    //stores id of the employee
     public id?:number;
+    //stores name of the employee
     public name:string;
+    //stores age of the employee
     public age:number;
+    //stores storeId of the employee
     public storeId:number;
+    //stores password of the employee
     public password:string;
+    //stores mobileNumber of the employee
     public mobileNumber:string;
+    //stores email of the employee
     public email:string;
+    //stores city of the employee
     public city:string;
     
 
-    constructor(
-        id:number,
-        name:string,
-        age:number,
-        storeId:number,
-        password:string,
-        mobileNumber:string,
-        email:string,
-        city:string){
+    //initialise properties of the class through constructor
+    constructor(id:number, name:string, age:number, storeId:number, password:string,
+                 mobileNumber:string, email:string, city:string)
+    {
             this.id = id;
             this.name = name;
             this.city = city;
